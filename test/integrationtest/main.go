bb90cb0c1941ff19dc2b0dc41c1ad320ebccc264
Luca409 <lucaloncar@gmail.com>
2021-05-09 21:52:49 -0400

    working on automating itests
    rebase
    
    todo: lower permission token, run on push to staging
    
    rebase

--- test/integrationtest/main.go
+++ test/integrationtest/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"seneca/env"
 	"seneca/test/integrationtest/syncer"
 )
 
 func main() {
+	if err := env.ValidateEnvironmentVariables(); err != nil {
+		log.Fatalf("Failed to ValidateEnvironmentVariables: %s", err.Error())
+	}
+
 	if err := syncer.E2ESyncer("senecacam-staging"); err != nil {
-		log.Fatal(fmt.Sprintf("SyncerTest failed: %s", err.Error()))
+		log.Fatalf("SyncerTest failed: %s", err.Error())
 	}
 }
