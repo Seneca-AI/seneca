3b8d6c830a48a1be5433be434e2bb6255a83ffa4
Luca409 <lucaloncar@gmail.com>
2021-05-12 02:19:16 -0400

    implemented tripDAO, eventDAO, drivingConditionDAO - TODO: test manual, unit test

---
+++ internal/dao/tripdao/tripdao.go
@@ -0,0 +1,164 @@
+package tripdao
+
+import (
+	"context"
+	"fmt"
+	"seneca/api/constants"
+	"seneca/api/senecaerror"
+	st "seneca/api/type"
+	"seneca/internal/client/cloud"
+	"seneca/internal/dao"
+	"seneca/internal/util"
+	"time"
+)
+
+const (
+	userIDFieldName    = "UserId"
+	startTimeFieldName = "StartTimeMs"
+	endTimeFieldName   = "EndTimeMs"
+)
+
+type SQLTripDAO struct {
+	sql dao.SQLInterface
+}
+
+func (tdao *SQLTripDAO) CreateUniqueTrip(ctx context.Context, trip *st.TripInternal) (*st.TripInternal, error) {
+	// See if there are any existing overlapping trips.
+	tripIDs, err := tdao.ListUserTripIDsByTime(trip.UserId, util.MillisecondsToTime(trip.StartTimeMs), util.MillisecondsToTime(trip.EndTimeMs))
+	if err != nil {
+		return nil, fmt.Errorf("error checking for existing trips: %w", err)
+	}
+
+	if len(tripIDs) > 0 {
+		return nil, fmt.Errorf("overlapping trip already exists")
+	}
+
+	tripID, err := tdao.sql.Create(constants.TripTable, trip)
+	if err != nil {
+		return nil, fmt.Errorf("error creating trip %v - err: %w", trip, err)
+	}
+
+	trip.Id = tripID
+	if err := tdao.PutTripByID(context.TODO(), trip.Id, trip); err != nil {
+		return nil, fmt.Errorf("error patching trip's ID %q - err: %w", trip.Id, err)
+	}
+
+	return trip, nil
+}
+
+func (tdao *SQLTripDAO) GetTripByID(ctx context.Context, userID, tripID string) (*st.TripInternal, error) {
+	tripObj, err := tdao.sql.GetByID(constants.TripTable, tripID)
+	if err != nil {
+		return nil, fmt.Errorf("error getting trip from strore: %w", err)
+	}
+
+	trip, ok := tripObj.(*st.TripInternal)
+	if !ok {
+		return nil, fmt.Errorf("expected type TripInternal, got %T", tripObj)
+	}
+
+	if trip.UserId != userID {
+		return nil, senecaerror.NewBadStateError(fmt.Errorf("mistmatch between UserID and tripID.UserID (UserID, tripID.UserID)(%s, %s)", userID, trip.UserId))
+	}
+
+	return trip, nil
+}
+
+func (tdao *SQLTripDAO) PutTripByID(ctx context.Context, tripID string, trip *st.TripInternal) error {
+	return tdao.sql.Insert(constants.TripTable, tripID, trip)
+}
+
+func (tdao *SQLTripDAO) ListUserTripIDs(userID string) ([]string, error) {
+	return tdao.sql.ListIDs(constants.TripTable, []*cloud.QueryParam{{FieldName: userIDFieldName, Operand: "=", Value: userID}})
+}
+
+func (tdao *SQLTripDAO) ListUserTripIDsByTime(userID string, startTime time.Time, endTime time.Time) ([]string, error) {
+	// { [ } ] or [ {} ] -- [] are query bounds, {} are trips
+	overLappingStartQuery := []*cloud.QueryParam{
+		{
+			FieldName: userIDFieldName,
+			Operand:   "=",
+			Value:     userID,
+		},
+		{
+			FieldName: endTimeFieldName,
+			Operand:   ">=",
+			Value:     util.TimeToMilliseconds(startTime),
+		},
+		{
+			FieldName: endTimeFieldName,
+			Operand:   "<=",
+			Value:     util.TimeToMilliseconds(endTime),
+		},
+	}
+
+	// [ { ] } or [ {} ]
+	overLappingEndQuery := []*cloud.QueryParam{
+		{
+			FieldName: userIDFieldName,
+			Operand:   "=",
+			Value:     userID,
+		},
+		{
+			FieldName: startTimeFieldName,
+			Operand:   ">=",
+			Value:     util.TimeToMilliseconds(startTime),
+		},
+		{
+			FieldName: startTimeFieldName,
+			Operand:   "<=",
+			Value:     util.TimeToMilliseconds(endTime),
+		},
+	}
+	// { [] }
+	superQuery := []*cloud.QueryParam{
+		{
+			FieldName: userIDFieldName,
+			Operand:   "=",
+			Value:     userID,
+		},
+		{
+			FieldName: startTimeFieldName,
+			Operand:   "<=",
+			Value:     util.TimeToMilliseconds(startTime),
+		},
+		{
+			FieldName: endTimeFieldName,
+			Operand:   ">=",
+			Value:     util.TimeToMilliseconds(endTime),
+		},
+	}
+
+	overLappingStartIDs, err := tdao.sql.ListIDs(constants.TripTable, overLappingStartQuery)
+	if err != nil {
+		return nil, fmt.Errorf("error listing tripIDs between %q and %q for user %q: %w", startTime, endTime, userID, err)
+	}
+	overLappingEndIDs, err := tdao.sql.ListIDs(constants.TripTable, overLappingEndQuery)
+	if err != nil {
+		return nil, fmt.Errorf("error listing tripIDs between %q and %q for user %q: %w", startTime, endTime, userID, err)
+	}
+	superIDs, err := tdao.sql.ListIDs(constants.TripTable, superQuery)
+	if err != nil {
+		return nil, fmt.Errorf("error listing tripIDs between %q and %q for user %q: %w", startTime, endTime, userID, err)
+	}
+
+	uniqueTripIDs := map[string]bool{}
+	for _, sid := range overLappingStartIDs {
+		uniqueTripIDs[sid] = true
+	}
+	for _, eid := range overLappingEndIDs {
+		uniqueTripIDs[eid] = true
+	}
+	for _, sid := range superIDs {
+		uniqueTripIDs[sid] = true
+	}
+	allTripIDs := []string{}
+	for k := range uniqueTripIDs {
+		allTripIDs = append(allTripIDs, k)
+	}
+
+	return allTripIDs, nil
+}
+func (tdao *SQLTripDAO) DeleteTripByID(ctx context.Context, tripID string) error {
+	return tdao.sql.DeleteByID(constants.TripTable, tripID)
+}
