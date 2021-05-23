2bbb5009579d2cf6202bea10defa815305192ea0
Luca409 <lucaloncar@gmail.com>
2021-05-23 11:16:58 -0400

    create misc folder (#88)

---
+++ misc/dev_tools/google_cloud_datastore/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	st "seneca/api/type"
+	"seneca/internal/client/cloud/gcp/datastore"
+	"seneca/internal/dao/rawlocationdao"
+	"seneca/internal/dao/rawmotiondao"
+	"seneca/internal/dao/rawvideodao"
+	"seneca/internal/dao/userdao"
+	"time"
+)
+
+func insertUserWithToken(sqlService *datastore.Service, email, pathToOauthToken string) {
+	userDAO := userdao.NewSQLUserDAO(sqlService)
+
+	oauthFile, err := os.Open(pathToOauthToken)
+	if err != nil {
+		log.Fatalf("Error opening token file %q - err: %v", pathToOauthToken, err)
+	}
+	defer oauthFile.Close()
+
+	bytes, err := ioutil.ReadAll(oauthFile)
+	if err != nil {
+		log.Fatalf("Error reading token file %q - err: %v", pathToOauthToken, err)
+	}
+
+	user := &st.User{
+		Email:      email,
+		OauthToken: bytes,
+	}
+
+	_, err = userDAO.InsertUniqueUser(user)
+	if err != nil {
+		log.Fatalf("Error inserting user err: %v", err)
+	}
+}
+
+func deleteAllRawVideosForUser(sqlService *datastore.Service, userID string) error {
+	rawVideoDAO := rawvideodao.NewSQLRawVideoDAO(sqlService, time.Second*5)
+
+	rawVideoIDs, err := rawVideoDAO.ListUserRawVideoIDs(userID)
+	if err != nil {
+		return err
+	}
+
+	for _, rid := range rawVideoIDs {
+		if err := rawVideoDAO.DeleteRawVideoByID(rid); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func deleteAllRawLocationsForUser(sqlService *datastore.Service, userID string) error {
+	rawLocationDAO := rawlocationdao.NewSQLRawLocationDAO(sqlService)
+
+	rawLocationIDs, err := rawLocationDAO.ListUserRawLocationIDs(userID)
+	if err != nil {
+		return err
+	}
+
+	for _, rlid := range rawLocationIDs {
+		if err := rawLocationDAO.DeleteRawLocationByID(rlid); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func deleteAllRawMotionsForUser(sqlService *datastore.Service, userID string) error {
+	rawMotionDAO := rawmotiondao.NewSQLRawMotionDAO(sqlService)
+
+	rawMotionIDs, err := rawMotionDAO.ListUserRawMotionIDs(userID)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Got %d rawMotions\n", len(rawMotionIDs))
+	for _, rlid := range rawMotionIDs {
+		if err := rawMotionDAO.DeleteRawMotionByID(rlid); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// For interfacing with cloud datastore.
+func main() {
+	sqlService, err := datastore.New(context.TODO(), "senecacam-staging")
+	if err != nil {
+		log.Fatalf("Error initializing datastore service %v", err)
+	}
+
+	insertUserWithToken(sqlService, "itestuser000@senecacam.com", "token.json")
+}
---
+++ misc/trashbin/cmd/datagatherer/main.go
@@ -0,0 +1,68 @@
+package main
+
+// import (
+// 	"context"
+// 	"fmt"
+// 	"log"
+// 	"net/http"
+// 	"os"
+// 	"time"
+
+// 	"seneca/internal/client/cloud/gcp"
+// 	"seneca/internal/client/logging"
+// 	"seneca/internal/datagatherer/rawvideohandler"
+// 	"seneca/internal/util/mp4"
+// )
+
+// const (
+// 	port                 = "8080"
+// 	rawVideoEndpointPath = "rawvideo"
+// 	cutVideoEndpointPath = "cutvideo"
+// )
+
+// // TODO: make this configurable in different envs.
+// func main() {
+// 	ctx := context.TODO()
+// 	projectID := os.Getenv("GOOGLE_CLOUD_PROJECT")
+// 	if projectID == "" {
+// 		fmt.Fprintf(os.Stderr, "GOOGLE_CLOUD_PROJECT environment variable must be set.\n")
+// 		os.Exit(1)
+// 	}
+
+// 	logger, err := logging.NewGCPLogger(ctx, "datagatherer", projectID)
+// 	if err != nil {
+// 		fmt.Printf("logging.NewGCPLogger() returns - err: %v", err)
+// 		return
+// 	}
+
+// 	gcsc, err := gcp.NewGoogleCloudStorageClient(ctx, projectID, time.Second*10, time.Minute)
+// 	if err != nil {
+// 		logger.Critical(fmt.Sprintf("cloud.NewGoogleCloudStorageClient() returns - err: %v", err))
+// 		return
+// 	}
+
+// 	gcsd, err := gcp.NewGoogleCloudDatastoreClient(ctx, projectID, time.Second)
+// 	if err != nil {
+// 		logger.Critical(fmt.Sprintf("cloud.NewGoogleCloudDatastoreClient() returns - err: %v", err))
+// 		return
+// 	}
+
+// 	mp4Tool, err := mp4.NewMP4Tool(logger)
+// 	if err != nil {
+// 		logger.Critical(fmt.Sprintf("mp4.NewMP4Tool() returns - err: %v", err))
+// 		return
+// 	}
+
+// 	rawVideoHandler, err := rawvideohandler.NewRawVideoHandler(gcsc, gcsd, mp4Tool, logger, projectID)
+// 	if err != nil {
+// 		logger.Critical(fmt.Sprintf("cloud.NewRawVideoHandler() returns - err: %v", err))
+// 		return
+// 	}
+
+// 	http.HandleFunc(fmt.Sprintf("/%s", rawVideoEndpointPath), rawVideoHandler.HandleRawVideoHTTPRequest)
+
+// 	fmt.Printf("Starting server at port %s\n", port)
+// 	if err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil); err != nil {
+// 		log.Fatal(err)
+// 	}
+// }
